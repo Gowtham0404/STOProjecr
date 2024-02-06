cafcd9c2aa9746059e94ea79c77577c8006651a5
Gowtham <kkgkgowtham@gmail.com>
2024-02-06 18:11:18 +0530

    good file

---
+++ config.py
@@ -0,0 +1,12 @@
+# config.py
+
+params = {
+    'mongo_host': 'localhost',
+    'mongo_port': 27017,
+    'db_name': 'admin',
+    'collection_name': 'emaildatafeeder',
+    'last_timestamp_collection_name': 'lasttimestamp',
+    'cohort_all_collection': 'cohort_all_id',
+    'cohort_collection': 'cohortid'
+    
+}
